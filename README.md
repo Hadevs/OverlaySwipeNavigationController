2b7ff842b91ebc7f10f8c1da4fd31418d670adcc
Danil Kovalev <kovalev@softo.co>
2022-11-16 04:02:24 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OverlaySwipeNavigationController
+
+A description of this package.
---
+++ Sources/OverlaySwipeNavigationController/OverlaySwipeNavigationController.swift
@@ -0,0 +1,6 @@
+public struct OverlaySwipeNavigationController {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
